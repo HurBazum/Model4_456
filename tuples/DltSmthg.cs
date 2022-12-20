ad5061c75240df9265a91ef313333e51cef719cb
Anton Spiridonov <hoorbazoom@gmail.com>
2022-12-21 01:48:18 +0700

    Добавьте файлы проекта.

---
+++ tuples/DltSmthg.cs
@@ -0,0 +1,22 @@
+﻿namespace DeleteLib
+{
+    public static class DltSmthg
+    {
+        public static void _setCursorPosition(out int xPositionOfCursorBeforeEnter, out int yPositionOfCursorBeforeEnter)
+        {
+            xPositionOfCursorBeforeEnter = Console.CursorLeft;
+            yPositionOfCursorBeforeEnter = Console.CursorTop;
+        }
+        public static void deleteWrongEnter(int xPositionOfCursorBeforeEnter, int yPositionOfCursorBeforeEnter, string? s)
+        {
+            string s0 = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                s0 += " ";
+            }
+            Console.SetCursorPosition(xPositionOfCursorBeforeEnter, yPositionOfCursorBeforeEnter);
+            Console.Write(s0);
+            Console.SetCursorPosition(xPositionOfCursorBeforeEnter, yPositionOfCursorBeforeEnter);
+        }
+    }
+}
---
+++ tuples/Program.cs
@@ -0,0 +1,52 @@
+﻿using DeleteLib;
+
+namespace tuples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool operationResult = false;
+			string enter = "";
+
+			(string name, string surname, byte age, string login, byte loginLength, bool havingPet, string[] favColors)[] User = new (string, string, byte, string, byte, bool, string[])[3];
+			for (int j = 0; j < User.Length; j++)
+			{
+				Console.WriteLine($"Анкета юзера #{j}:");
+				Console.Write("Введите имя: ");
+				User[j].name = Console.ReadLine();
+				Console.Write("Введите фамилию: ");
+				User[j].surname = Console.ReadLine();
+				Console.Write("Введите логин: ");
+				User[j].login = Console.ReadLine();
+				User[j].loginLength = (byte)User[j].login.Length;
+				Console.WriteLine("Есть ли у Вас животные? Да или Нет");
+				User[j].havingPet = (string.Compare(Console.ReadLine(), "Да", true) == 0) ? true : false;
+				Console.Write("Введите возраст: ");
+				User[j].age = byte.Parse(Console.ReadLine());
+				User[j].favColors = new string[3];
+				Console.WriteLine("Введите три Ваших любимых цвета(по английски с большой буквы):");
+				for (int i = 0; i < User[j].favColors.Length; i++)
+				{
+					Console.Write($"Цвет #{i}: ");
+					DltSmthg._setCursorPosition(out int x, out int y);
+					do
+					{
+						enter = Console.ReadLine();
+						if (Enum.IsDefined(typeof(ConsoleColor), enter))
+						{
+							User[j].favColors[i] = enter;
+							operationResult = true;
+						}
+						else
+						{
+							DltSmthg.deleteWrongEnter(x, y, enter);
+							operationResult = false;
+						}
+					} while (!operationResult);
+				}
+				Console.WriteLine("/*********************************************************/");
+			}
+		}
+	}
+}
